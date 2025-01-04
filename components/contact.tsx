62c6671deaac77df8b91c6aa4dc00b59f360741b
Padia <padia@Nikhils-MacBook-Air.local>
2025-01-04 18:55:30 +0530

    build successfull

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,17 +32,17 @@ export default function Contact() {
       <SectionHeading>Contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
+        Please contact me directly at
         <a className="underline" href="mailto:example@gmail.com">
           example@gmail.com
-        </a>{" "}
+        </a>
         or through this form.
       </p>
 
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
-          const { data, error } = await sendEmail(formData);
+          const { error } = await sendEmail(formData);
 
           if (error) {
             toast.error(error);
