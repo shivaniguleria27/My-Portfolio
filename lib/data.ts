70af8faed1de3f4e774a8e10a9645fc3981b0829
Shivani <padia@Mac.mshome.net>
2025-01-06 12:18:06 +0530

    fixed build error

--- lib/data.ts
+++ lib/data.ts
@@ -1,11 +1,9 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import portfImg from "@/public/portf.png";
 import corpcommentImg from "@/public/corpcomment.png";
 import gamingImg from "@/public/gaming.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
